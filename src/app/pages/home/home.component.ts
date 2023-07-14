8e97429105ab6232b64af27c18c99007d12a7fdd
Ankur Jain <ankurankur123@gmail.com>
2023-07-14 13:38:31 +0530

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { BaseService } from 'src/app/services/base.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent  {
+export class HomeComponent {
+
+  constructor(private base: BaseService) {
+    this.base.reset.subscribe(item => {
+      if (item) {
+        this.message = {};
+        this.messages = [];
+        this.title = 'Lets start with upload something..';
+      }
+    })
+  }
 
   message = {};
-    messages: Array<any> = [];
+  title = 'Lets start with upload something..';
+  messages: Array<any> = [];
 
-
-
-    sendMessage(message: string) {
-        // if (message === "") return;
-        // this.messages.push({ 'class': 'message-sent', 'value': message });
-        // // this.messages.push({ 'class': 'message-received', 'value': 'eyo tryo againo lato m8o' });
-
-        // // Connect with chatbot api to receive a response
-        // this.chatbot.getResponse(message).subscribe((res: { [x: string]: any; }) => {
-        //     // Add response to list of messages
-        //     this.messages.push({ 'class': 'message-received', 'value': res['value'] });
-        // });
-    }
-
-    onKeyDown(event: any) { // without type info
-        this.sendMessage(event.target.value);
-    }
-
+  sendMessage(selectedFiles: any) {
+    const formData = new FormData();
+    formData.append("blobFile", ((selectedFiles as any)[0] as any));
+    this.title = 'Loading...'
+    this.base.getData('https://europe-west1-hack-team-netrai.cloudfunctions.net/uploader-function-1', formData).subscribe((item: any) => {
+      console.log(item);
+      this.message = {};
+      this.messages = [];
+      const msg = item.msg[0].predictions[0].structValue.fields.content.stringValue;
+      const finalObj = msg.split('\n\n');
+      this.title = finalObj[1] ? finalObj[0] : 'Summarize';
+      const val = finalObj[1] ? finalObj[1] : finalObj[0];
+      this.messages.push({ 'class': 'message-received', 'value': val });
+      this.base.resetCall(false);
+    });
+  }
 }
+
+
